9c084b0b51cdd6bfedd508acaa479b9ef6a797de
halaprix <panicz.klos@gmail.com>
2022-10-05 13:59:14 +0200

    divide in three

--- contracts/AccountFactory.sol
+++ contracts/AccountFactory.sol
@@ -10,6 +10,7 @@ import {IManager} from "./interfaces/IManager.sol";
 import {IServiceRegistry} from "./interfaces/IServiceRegistry.sol";
 import {Constants} from "./utils/Constants.sol";
 import {Clones} from "@openzeppelin/contracts/proxy/Clones.sol";
+import "hardhat/console.sol";
 
 contract AccountFactory is Constants {
     IServiceRegistry public immutable serviceRegistry;
@@ -21,7 +22,6 @@ contract AccountFactory is Constants {
     }
 
     mapping(address => Account[]) public accounts;
-    mapping(address => address) public migrated;
 
     address public immutable proxyTemplate;
     address public immutable self;
@@ -70,79 +70,6 @@ contract AccountFactory is Constants {
 
     function accountsCount(address user) public view returns (uint256) {
         return accounts[user].length;
-    }
-
-    modifier onlyDelegate() {
-        require(address(this) != self, "bot/only-delegate");
-        _;
-    }
-
-    /// @dev Returns the correct user proxy depending on the migration status
-    /// @param user address of the user
-    /// @return proxyAddr address of the users proxy
-    function getMakerProxy(address user)
-        public
-        view
-        returns (address proxyAddr)
-    {
-        IProxyRegistry mcdRegistry = IProxyRegistry(
-            serviceRegistry.getRegisteredService(PROXY_REGISTRY_KEY)
-        );
-        proxyAddr = mcdRegistry.proxies(user);
-        if (migrated[user] != address(0)) {
-            return migrated[user];
-        }
-
-        return proxyAddr;
-    }
-
-    /// @dev transfers ownership of all CdpIds to the newly created Oasis proxy
-    /// @param cdpIds list of user owned cdpIds
-    /// @return newProxy address of the users Oasis proxy
-    function migrateMaker(uint256[] calldata cdpIds)
-        public
-        onlyDelegate
-        returns (address newProxy)
-    {
-        require(migrated[msg.sender] == address(0), "factory/already-migrated");
-        IManager manager = IManager(
-            serviceRegistry.getRegisteredService(CDP_MANAGER_KEY)
-        );
-        AccountFactory _factory = AccountFactory(
-            serviceRegistry.getRegisteredService(ACCOUNT_FACTORY_KEY)
-        );
-        newProxy = _factory.createAccount(0, msg.sender);
-        uint256[] memory _cdpIds = cdpIds;
-        uint256 length = _cdpIds.length;
-        for (uint256 i; i < length; i++) {
-            manager.give(_cdpIds[i], newProxy);
-        }
-
-        return newProxy;
-    }
-
-    /// @dev transfers ownership of all CdpIds to the existing Oasis proxy
-    /// @param cdpIds list of user owned cdpIds
-    /// @return newProxy address of the users Oasis proxy
-    function migrateAdditionalVaults(uint256[] calldata cdpIds)
-        public
-        onlyDelegate
-        returns (address newProxy)
-    {
-        require(migrated[msg.sender] != address(0), "factory/already-migrated");
-        IManager manager = IManager(
-            serviceRegistry.getRegisteredService(CDP_MANAGER_KEY)
-        );
-        // assume the first proxy from new factory is the dedicated maker migration one
-        newProxy = migrated[msg.sender];
-        uint256[] memory _cdpIds = cdpIds;
-        uint256 length = _cdpIds.length;
-
-        for (uint256 i; i < length; i++) {
-            manager.give(_cdpIds[i], newProxy);
-        }
-
-        return newProxy;
     }
 
     event AccountCreated(
